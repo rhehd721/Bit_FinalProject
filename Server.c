af06aebc68955a4ca8bb7f42c7862c6940071ddb
알파카 <rhehd721@naver.com>
2021-01-25 23:24:54 +0900

    Server 주석추가

--- Server.c
+++ Server.c
@@ -14,6 +14,7 @@ void * handle_clnt(void * arg);
 void send_msg(char * msg, int len);
 void error_handling(char * msg);
 
+// 서버에 접속한 클라이언트의 소켓 관리를 위한 변수와 배열
 int clnt_cnt=0;
 int clnt_socks[MAX_CLNT];
 pthread_mutex_t mutx;
@@ -24,16 +25,18 @@ int main(int argc, char *argv[])
 	struct sockaddr_in serv_adr, clnt_adr;
 	int clnt_adr_sz;
 	pthread_t t_id;
+	// 파일을 실행시킬때 port번호를 주지않으면 실행시키지 말고 error 메세지를 출력해
 	if(argc!=2) {
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
   
 	pthread_mutex_init(&mutx, NULL);
+	// 서버는 IPv4(PF_INET) 프로토콜을 사용할거고 연결지향형(SOCK_STREAM) 소켓을 사용할거야
 	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
 
 	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family=AF_INET; 
+	serv_adr.sin_family=AF_INET;
 	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
 	serv_adr.sin_port=htons(atoi(argv[1]));
 	
@@ -48,10 +51,13 @@ int main(int argc, char *argv[])
 		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr,&clnt_adr_sz);
 		
 		pthread_mutex_lock(&mutx);
+		// 새로운 연결이 형성될 때마다 변수 clnt_cnt와 배열 clnt_sock에 해당 정보를 등록한다.
 		clnt_socks[clnt_cnt++]=clnt_sock;
 		pthread_mutex_unlock(&mutx);
 	
+		// 추가된 클라이언트에게 서비스를 제공하기 위한 쓰레드를 생성
 		pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
+		// pthread_detach 함수호출을 통해서 종료된 쓰레드가 메모리에서 완전히 소멸되도록 하고 있다.
 		pthread_detach(t_id);
 		printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
 	}
@@ -83,6 +89,7 @@ void * handle_clnt(void * arg)
 	close(clnt_sock);
 	return NULL;
 }
+// 연결된 모든 클라이언트에게 메세지를 전송하는 함수
 void send_msg(char * msg, int len)   // send to all
 {
 	int i;
