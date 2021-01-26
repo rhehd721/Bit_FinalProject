ea6ebad92e1c02943cbd8173b6e24dd10fa2d0be
B._.0721 <rhehd721@naver.com>
2021-01-26 17:45:36 +0900

    쓰레드 관련 주석 추가

--- Client.c
+++ Client.c
@@ -1,21 +1,23 @@
+// Client
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <pthread.h>
-	
+
 #define BUF_SIZE 100
 #define NAME_SIZE 20
-	
+
 void * send_msg(void * arg);
 void * recv_msg(void * arg);
 void error_handling(char * msg);
-	
+
 char name[NAME_SIZE]="[DEFAULT]";
 char msg[BUF_SIZE];
-	
+
 int main(int argc, char *argv[])
 {
 	int sock;
@@ -27,25 +29,35 @@ int main(int argc, char *argv[])
 		exit(1);
 	 }
 	
+	// 접속한 Client의 이름 출력
 	sprintf(name, "[%s]", argv[3]);
 	sock=socket(PF_INET, SOCK_STREAM, 0);
 	
+	// 주소 초기화
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family=AF_INET;
 	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
 	serv_addr.sin_port=htons(atoi(argv[2]));
-	  
-	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+	
+	// 소켓을 이용해 서버의 정보를 지닌 구조체를 가지고 접속 요청을 한다
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1){
 		error_handling("connect() error");
+	}
 	
 	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
 	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
+	// int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void*), void *arg)
+	// 1. thread : 성공적으로 함수가 호출되면 이곳에 thread ID가 저장됩니다. 이 인자로 넘어온 값을 통해서 pthread_join과 같은 함수를 사용할 수 있습니다.
+	// 2. attr : 스레드의 특성을 정의합니다. 기본적으로 NULL을 지정합니다. 만약 스레드의 속성을 지정하려고 한다면 pthread_attr_init등의 함수로 초기화해야합니다.
+	// 3. start_routine : 어떤 로직을 할지 함수 포인터를 매개변수로 받습니다. 
+	// 4. arg : start_routine에 전달될 인자를 말합니다. start_routine에서 이 인자를 변환하여 사용합니다.
+
 	pthread_join(snd_thread, &thread_return);
 	pthread_join(rcv_thread, &thread_return);
 	close(sock);  
 	return 0;
 }
-	
+
 void * send_msg(void * arg)   // send thread main
 {
 	int sock=*((int*)arg);
@@ -63,7 +75,7 @@ void * send_msg(void * arg)   // send thread main
 	}
 	return NULL;
 }
-	
+
 void * recv_msg(void * arg)   // read thread main
 {
 	int sock=*((int*)arg);
@@ -79,11 +91,10 @@ void * recv_msg(void * arg)   // read thread main
 	}
 	return NULL;
 }
-	
+
 void error_handling(char *msg)
 {
 	fputs(msg, stderr);
 	fputc('\n', stderr);
 	exit(1);
 }
-
--- Server.c
+++ Server.c
@@ -1,3 +1,5 @@
+// Server
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
