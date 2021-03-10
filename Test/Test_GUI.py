f2c8af2708ae0a33fcceee8357d8e73fd7d8ebfe
알파카 <rhehd721@naver.com>
2021-03-10 14:22:58 +0900

    python thread

--- Test/Test_GUI.py
+++ Test/Test_GUI.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*- 
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 import ctypes
-
+import threading
 
 
 class MyApp(QWidget):
@@ -10,6 +11,14 @@ class MyApp(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
+
+    def sum(self, low, high):
+        total = 0
+        for i in range(low, high):
+            total += i
+        print("Subthread", total)
+        while(1):
+            pass
 
     def initUI(self):
         
@@ -45,14 +54,32 @@ class MyApp(QWidget):
 
     # 동작 명령어를 눌렀을때 작동하는 부분
     def btnRun_clicked(self):
-        libc = ctypes.CDLL("./test.so")
-        main = libc.main("127.0.0.1".encode('utf-8'), "8080".encode('utf-8'), "park".encode('utf-8'))
+        # libc = ctypes.CDLL("./test.so")
+        # main = libc.main("127.0.0.1".encode('utf-8'), "8080".encode('utf-8'), "park".encode('utf-8'))
         self.label.setText("?")
         self.label.repaint()
+
+        t = threading.Thread(target=self.sum, args=(1, 100000))
+        t.start()
+        # self.sum(1, 10000)
         # C파일 불러오기
-        counter = libc.Event_Start()
+        # counter = libc.Event_Start()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
     sys.exit(app.exec_())
+
+
+# import threading
+ 
+# def sum(low, high):
+#     total = 0
+#     for i in range(low, high):
+#         total += i
+#     print("Subthread", total)
+ 
+# t = threading.Thread(target=sum, args=(1, 100000))
+# t.start()
+ 
+# print("Main Thread")
